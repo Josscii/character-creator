2ffdcecdcb770e0506e66ddfd92249a2fe47dda8
josscii <josscii@88.com>
2022-01-15 08:48:59 +0800

    solution

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+    <div style={{position: "fixed", bottom: 0, backgroundColor: "hsl(195deg, 20%, 86%)", height: "40%", width:"100%"}}></div>
       <CharacterEditor />
       <Footer />
     </>
